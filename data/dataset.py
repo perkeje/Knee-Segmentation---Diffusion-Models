Add gradient accumulation and some bugs fixed

data/dataset.py
@@ -54,11 +54,11 @@ class MriKneeDataset(data.Dataset):
                 self.latest_subject = self.transformations(self.latest_subject)
 
         raw_img = self.latest_subject.raw_img.data.squeeze()[slice_index, :, :]
-        raw_img = raw_img.unsqueeze(0).float()
+        raw_img = raw_img.unsqueeze(0).half()
         mask = F.one_hot(
             self.latest_subject.mask_img.data.squeeze().to(dtype=torch.int64)[slice_index, :, :],
             num_classes=6,
         )
-        mask = mask.permute(2, 0, 1).float()
+        mask = mask.permute(2, 0, 1).half()
 
         return raw_img, mask, slice_index

diffusion/gaussian_diffusion.py
@@ -24,7 +24,7 @@ class GaussianDiffusion(nn.Module):
         *,
         image_size,
         class_weights=torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]),
-        timesteps=500,
+        timesteps=100,
         objective="pred_x0",
         beta_schedule="linear",
         schedule_fn_kwargs={},
@@ -34,7 +34,7 @@ class GaussianDiffusion(nn.Module):
         assert not (type(self) is GaussianDiffusion and model.channels != model.out_dim)
         self.model = model
         self.channels = self.model.channels
-        self.class_weights = class_weights.to(next(self.model.parameters()).device)
+        self.class_weights = torch.log1p(class_weights)
         self.image_size = image_size
 
         self.objective = objective
@@ -215,7 +215,7 @@ class GaussianDiffusion(nn.Module):
         return_all_timesteps=False,
         disable_bar=False,
     ):
-        device = self.model.device
+        device = raw.device
 
         segmentation = torch.randn(
             (batch_size, self.channels, self.image_size, self.image_size), device=device
@@ -247,6 +247,8 @@ class GaussianDiffusion(nn.Module):
         )
 
     def p_losses(self, raw, x_start, t, noise=None):
+        if self.class_weights.device != next(self.model.parameters()).device:
+            self.class_weights = self.class_weights.to(next(self.model.parameters()).device)
         _, c, _, _ = x_start.shape
 
         # Ensure the number of channels (c) matches the number of classes

experiments/trainer.py
@@ -32,6 +32,7 @@ class Trainer:
         save_and_sample_every=20,
         es_patience=10,
         lr_patience=5,
+        gradient_accumulation_steps=4,
         results_folder="./results",
         checkpoint_folder="./results/checkpoints",
         best_checkpoint="best_checkpoint",
@@ -41,7 +42,7 @@ class Trainer:
         super().__init__()
         set_seed(42)
 
-        self.accelerator = Accelerator()
+        self.accelerator = Accelerator(gradient_accumulation_steps=gradient_accumulation_steps)
 
         self.save_and_sample_every = save_and_sample_every
         self.batch_size = batch_size
@@ -72,7 +73,7 @@ class Trainer:
         self.train_dl = DataLoader(
             self.train_ds,
             batch_size=self.batch_size,
-            shuffle=True,
+            shuffle=False,
             pin_memory=True,
             num_workers=4 * self.accelerator.num_processes,
         )
@@ -80,7 +81,7 @@ class Trainer:
         self.val_dl = DataLoader(
             self.val_ds,
             batch_size=self.batch_size,
-            shuffle=True,
+            shuffle=False,
             pin_memory=True,
             num_workers=4 * self.accelerator.num_processes,
         )
@@ -173,7 +174,7 @@ class Trainer:
     def train(self):
         for epoch in range(self.step, self.epochs):
             self.model.train()
-            print("\n")
+            self.accelerator.print("\n")
             losses = []
             with tqdm(
                 total=len(self.train_dl),
@@ -185,26 +186,29 @@ class Trainer:
                     self.optimizer.zero_grad()
                     segmentation = data[1]
                     raw = data[0]
-                    with self.accelerator.autocast():
-                        loss = self.model(segmentation, raw)
+                    with self.accelerator.accumulate(self.model):
+                        with self.accelerator.autocast():
+                            loss = self.model(segmentation, raw)
 
-                    self.accelerator.backward(loss)
-                    self.accelerator.clip_grad_norm_(self.model.parameters(), 1.0)
-                    self.optimizer.step()
-                    if self.accelerator.is_main_process:
-                        train_bar.update(1)
-                        train_bar.set_postfix(
-                            train_loss=loss.item(),
-                            lr=self.scheduler.get_last_lr()[0] / self.accelerator.num_processes,
-                            gpu_lr=self.scheduler.get_last_lr()[0],
-                        )
-                    losses.append(loss.item())
+                        self.accelerator.backward(loss)
+                        if self.accelerator.sync_gradients:
+                            self.accelerator.clip_grad_norm_(self.model.parameters(), 1.0)
+                        self.optimizer.step()
+                        if self.accelerator.is_main_process:
+                            train_bar.update(1)
+                            train_bar.set_postfix(
+                                train_loss=loss.item(),
+                                lr=self.scheduler.get_last_lr()[0]
+                                / self.accelerator.num_processes,
+                                gpu_lr=self.scheduler.get_last_lr()[0],
+                            )
+                        losses.append(loss.item())
                 self.accelerator.wait_for_everyone()
-                losses = torch.tensor(losses)
-                loss = self.accelerator.gather(losses)
+                losses = torch.tensor(losses).to(self.accelerator.device)
+                loss = self.accelerator.gather(losses).mean().item()
                 if self.accelerator.is_main_process:
                     train_bar.set_postfix(
-                        train_loss=loss.mean().item(),
+                        train_loss=loss,
                         lr=self.scheduler.get_last_lr()[0] / self.accelerator.num_processes,
                         gpu_lr=self.scheduler.get_last_lr()[0],
                     )
@@ -233,11 +237,11 @@ class Trainer:
                             segmentation = data[1]
                             raw = data[0]
                             with self.accelerator.autocast():
-                                sampled = self.model.sample(
+                                sampled = self.model.module.sample(
                                     raw=raw, batch_size=self.batch_size, disable_bar=True
                                 )
-                            pred_mask_flat = torch.argmax(sampled, dim=1).flatten()
-                            true_mask_flat = torch.argmax(segmentation, dim=1).flatten()
+                            pred_mask_flat = torch.argmax(sampled, dim=1).flatten().cpu()
+                            true_mask_flat = torch.argmax(segmentation, dim=1).flatten().cpu()
 
                             f1 = f1_score(true_mask_flat, pred_mask_flat, average="micro")
                             iou = jaccard_score(
@@ -250,19 +254,20 @@ class Trainer:
                             val_bar.update(1)
 
                         self.accelerator.wait_for_everyone()
-                        val_loss = torch.tensor(val_loss)
-                        f1_list = torch.tensor(f1_list)
-                        iou_list = torch.tensor(iou_list)
+                        val_loss = torch.tensor(val_loss).to(self.accelerator.device)
+                        f1_list = torch.tensor(f1_list).to(self.accelerator.device)
+                        iou_list = torch.tensor(iou_list).to(self.accelerator.device)
 
                         val_loss = self.accelerator.gather(val_loss)
                         f1 = self.accelerator.gather(f1_list)
                         iou = self.accelerator.gather(iou_list)
 
+                        val_loss = val_loss.mean().item()
+                        f1 = f1.mean().item()
+                        iou = iou.mean().item()
+                        metric = f1 + iou
+
                         if self.accelerator.is_main_process:
-                            val_loss = val_loss.mean().item()
-                            f1 = f1.mean().item()
-                            iou = iou.mean().item()
-                            metric = f1 + iou
                             val_bar.set_postfix(val_loss=val_loss, f1=f1, iou=iou, metric=metric)
                             if metric > self.best_metric:
                                 self.best_metric = metric
@@ -274,7 +279,7 @@ class Trainer:
                             self.append_to_log(
                                 self.step,
                                 self.scheduler.get_last_lr()[0] / self.accelerator.num_processes,
-                                loss.item(),
+                                loss,
                                 val_loss,
                                 metric,
                                 iou,
@@ -295,7 +300,7 @@ class Trainer:
                     validation_sample = self.test_ds[80]
                     raw = validation_sample[0].unsqueeze(0)
                     with self.accelerator.autocast():
-                        sampled_images = self.model.sample(raw=raw, batch_size=1)
+                        sampled_images = self.model.module.sample(raw=raw, batch_size=1)
 
                     # Apply argmax and coloring
                     colored_sampled_images = apply_argmax_and_coloring(sampled_images) / 255.0

train.py
@@ -16,7 +16,6 @@ def load_or_compute_mean_std(data_dir):
     if os.path.exists(mean_std_path):
         mean_std = torch.load(mean_std_path)
         mean, std = mean_std["mean"], mean_std["std"]
-        print("Loaded mean and std from file.")
     else:
         print("Mean and std not found. You need to calculate these with pretrain.py first.")
         sys.exit(1)
@@ -29,7 +28,6 @@ def load_or_compute_class_weights(data_dir):
 
     if os.path.exists(class_weights_path):
         class_weights = torch.load(class_weights_path)
-        print("Loaded class weights from file.")
     else:
         print("Class weights not found. You need to calculate these with pretrain.py first.")
         sys.exit(1)
@@ -41,14 +39,8 @@ if __name__ == "__main__":
     params_dir = "./results/params"
 
     mean, std = load_or_compute_mean_std(params_dir)
-    print("Mean and std:")
-    print(mean, std)
-
     model = Unet(dim=32, dim_mults=(1, 2, 4, 8, 16), norm_mean=mean, norm_std=std)
-
     class_weights = load_or_compute_class_weights(params_dir)
-    print("Class weights:")
-    print(class_weights)
 
     diffusion = GaussianDiffusion(
         model,
@@ -63,13 +55,13 @@ if __name__ == "__main__":
         train_images_folder="./data/splitted/train",
         test_segmentations_folder="./data/splitted/test_masks",
         test_images_folder="./data/splitted/test",
-        batch_size=16,
+        batch_size=8,
         val_size=0.4,
         val_metric_size=4,
         lr=1e-4,
         epochs=250,
         adam_betas=(0.9, 0.99),
-        save_and_sample_every=20,
+        save_and_sample_every=5,
         es_patience=6,
         lr_patience=3,
         results_folder="./results",