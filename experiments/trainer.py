Fixed weights calculation and metrics averaging

experiments/trainer.py
@@ -243,11 +243,11 @@ class Trainer:
                             pred_mask_flat = torch.argmax(sampled, dim=1).flatten().cpu()
                             true_mask_flat = torch.argmax(segmentation, dim=1).flatten().cpu()
 
-                            f1 = f1_score(true_mask_flat, pred_mask_flat, average="micro")
+                            f1 = f1_score(true_mask_flat, pred_mask_flat, average="macro")
                             iou = jaccard_score(
                                 true_mask_flat,
                                 pred_mask_flat,
-                                average="micro",
+                                average="macro",
                             )
                             f1_list.append(f1)
                             iou_list.append(iou)

train.py
@@ -1,6 +1,7 @@
 import sys
 import os
-import torch
+
+from utils import load_mean_std, load_class_weights
 
 # Add the root directory of the project to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -10,37 +11,12 @@ from experiments.trainer import Trainer
 from unet.unet import Unet
 
 
-def load_or_compute_mean_std(data_dir):
-    mean_std_path = os.path.join(data_dir, "mean_std.pt")
-
-    if os.path.exists(mean_std_path):
-        mean_std = torch.load(mean_std_path)
-        mean, std = mean_std["mean"], mean_std["std"]
-    else:
-        print("Mean and std not found. You need to calculate these with pretrain.py first.")
-        sys.exit(1)
-
-    return mean, std
-
-
-def load_or_compute_class_weights(data_dir):
-    class_weights_path = os.path.join(data_dir, "class_weights.pt")
-
-    if os.path.exists(class_weights_path):
-        class_weights = torch.load(class_weights_path)
-    else:
-        print("Class weights not found. You need to calculate these with pretrain.py first.")
-        sys.exit(1)
-
-    return class_weights
-
-
 if __name__ == "__main__":
     params_dir = "./results/params"
 
-    mean, std = load_or_compute_mean_std(params_dir)
+    mean, std = load_mean_std(params_dir)
     model = Unet(dim=32, dim_mults=(1, 2, 4, 8, 16), norm_mean=mean, norm_std=std)
-    class_weights = load_or_compute_class_weights(params_dir)
+    class_weights = load_class_weights(params_dir)
 
     diffusion = GaussianDiffusion(
         model,

utils/preprocessing.py
@@ -30,25 +30,30 @@ def compute_mean_std(adapt_dir, exts=["nii", "nii.gz"]):
     paths = []
     for ext in exts:
         files = glob.glob(os.path.join(adapt_dir, "**", f"*.{ext}"), recursive=True)
-        paths.extend([os.path.basename(p) for p in files])
+        paths.extend(files)
 
-    mean = 0.0
-    std = 0.0
-    M2 = 0.0
-    count = 0
+    mean = torch.tensor(0.0, dtype=torch.double)
+    M2 = torch.tensor(0.0, dtype=torch.double)
+    count = torch.tensor(0, dtype=torch.double)
 
     print("Calculating dataset mean and std...")
     for path in tqdm(paths):
-        img = tio.ScalarImage(os.path.join(adapt_dir, path)).data.squeeze()
+        img = tio.ScalarImage(path).data.squeeze().flatten()
         img = img.to(dtype=torch.double)
-        img_mean = img.mean()
-        img_M2 = img.var()
-        delta = img_mean - mean
-        mean += delta / (count + 1)
-        M2 += img_M2 + delta**2 * count / (count + 1)
-        count += 1
+        batch_count = img.numel()
 
-    std = (M2 / count) ** 0.5
+        batch_mean = img.mean()
+        delta = batch_mean - mean
+
+        count += batch_count
+        mean += delta * batch_count / count
+        M2 += (
+            img.var(unbiased=False) * batch_count
+            + delta**2 * batch_count * (count - batch_count) / count
+        )
+
+    variance = M2 / count
+    std = torch.sqrt(variance)
 
     return mean, std
 
@@ -66,6 +71,5 @@ def calculate_class_weights(adapt_dir, num_classes=6, exts=["nii", "nii.gz"]):
         for i in range(num_classes):
             class_counts[i] += torch.sum(seg == i)
 
-    total_pixels = class_counts.sum()
-    class_weights = total_pixels / (num_classes * class_counts)
-    return class_weights.clone().detach()
+    class_weights = 1 / (class_counts / class_counts.sum())
+    return class_weights